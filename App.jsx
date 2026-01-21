3ab68e5df97a84b610749839e11b9c573361e40a
Supreme Technology <supremetechy@gmail.com>
2026-01-20 17:41:20 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import SRPBillSimulator from './components/SRPBillSimulator'
+import './App.css'
+
+function App() {
+  return(
+    <div className="App bg-slate-900 min-h-screen text-slate-200 flex items-center justify-center p-4">
+      <SRPBillSimulator />
+    </div>
+  )
+  
+}
+
+export default App
